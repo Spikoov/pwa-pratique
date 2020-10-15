5f81419d5ce1c15c55782be93dde1f395d4840c1
Spikoov <hem.jyh@gmail.com>
2020-10-15 11:25:16 +0200

    8. Notifications Push - résultat

--- sw.js
+++ sw.js
@@ -104,7 +104,7 @@ self.addEventListener('fetch', (evt) => {
     );
 });
 
-// 7.3 Notifications persistantes (envoyées depuis le service worker)
+/* 7.3 Notifications persistantes (envoyées depuis le service worker)
 // Affichage de la notification
 self.registration.showNotification("Notification du SW", {
     body:"je suis une notification dite persistante",
@@ -135,4 +135,19 @@ self.addEventListener("notificationclick", evt => {
 // Ecoute de l'événement close
 self.addEventListener("notificationclose", evt => {
     console.log("Notification fermée", evt);
+})*/
+
+	
+// 8.1 Intercepter une notification push
+self.addEventListener("push", evt => {
+    console.log("push event", evt);
+    console.log("data envoyée par la push notification :", evt.data.text());
+ 
+    // 8.1 afficher son contenu dans une notification
+    const title = evt.data.text();
+    const objNotification = {
+        body: "ça fonctionne", 
+        icon : "images/icons/icon-72x72.png"
+    };
+    self.registration.showNotification(title, objNotification);
 })
